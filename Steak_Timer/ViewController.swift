b6d798f1ccd0eb218116322744c5c75c7864a962
rojoMojoGit <sheickitout@gmail.com>
2021-03-09 01:19:16 -0500

    adding view and controller

--- Steak_Timer/ViewController.swift
+++ Steak_Timer/ViewController.swift
@@ -8,12 +8,60 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var steakTimer = ["rare":3, "medium":7, "wellDone":8]
+    
+    @IBAction func rareImageTapped(_ sender: Any) {
+        //test
+        print("rare")
+    }
+    @IBAction func mediumImageTapped(_ sender: Any) {
+        print("medium")
+    }
+    @IBAction func wellDoneImageTaped(_ sender: Any) {
+       
+        print("well-done")
+    }
+    
+    
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let timer = Timer(timeInterval: 1.0, repeats: true) { _ in print("DING DING!") }
     }
+    
+    @objc func updateTimer() {
+        if startTime < totalTime {
+            startTime += 1
+            progressLabel.progress = Float((startTime))/Float((totalTime))
+        }
+        else {
+            outputLabel.text = "Done"
+            let path = Bundle.main.path(forResource: "ring", ofType: "wav")!
+                let url = URL(fileURLWithPath: path)
+            do {
+                let sound = try AVAudioPlayer(contentsOf: url)
+                player = sound
+                sound.play()
+            } catch {
+                //
+            }
+            timer.invalidate()
+            
+        }
+        
+    }
+        
 
+   //Steak Timer-> 5,10,15
+    
+    
+    
+    
 
+//look for a countdown timer on stack over flow
 }
 
